b80b9c8a0c09059cc6cce31b22bd3f8de61951b3
Konrad S <konrad.t.szmit@gmail.com>
2019-03-18 20:20:56 +0100

    Added constructor, getters for PasswordGeneratorParameters.

--- src/main/java/pl/umcs/passwordgeneratorapi/PasswordGeneratorParameters.java
+++ src/main/java/pl/umcs/passwordgeneratorapi/PasswordGeneratorParameters.java
@@ -1,11 +1,38 @@
 package pl.umcs.passwordgeneratorapi;
 
 public class PasswordGeneratorParameters {
+    public PasswordGeneratorParameters(int passwordLength, boolean includeSymbols, boolean includeNumbers, boolean includeLowercaseCharacters, boolean includeUppercaseCharacters) {
+        this.passwordLength = passwordLength;
+        this.includeSymbols = includeSymbols;
+        this.includeNumbers = includeNumbers;
+        this.includeLowercaseCharacters = includeLowercaseCharacters;
+        this.includeUppercaseCharacters = includeUppercaseCharacters;
+    }
 
-    int passwordLength;
-    boolean includeSymbols;
-    boolean includeNumbers;
-    boolean includeLowercaseCharacters;
-    boolean includeUppercaseCharacters;
+    public int getPasswordLength() {
+        return passwordLength;
+    }
+
+    public boolean isIncludeSymbols() {
+        return includeSymbols;
+    }
+
+    public boolean isIncludeNumbers() {
+        return includeNumbers;
+    }
+
+    public boolean isIncludeLowercaseCharacters() {
+        return includeLowercaseCharacters;
+    }
+
+    public boolean isIncludeUppercaseCharacters() {
+        return includeUppercaseCharacters;
+    }
+
+    private int passwordLength;
+    private boolean includeSymbols;
+    private boolean includeNumbers;
+    private boolean includeLowercaseCharacters;
+    private boolean includeUppercaseCharacters;
 
 }
